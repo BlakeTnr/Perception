a7af81cbc5bfbc6189af5aa743536b20c37079d1
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-20 06:47:57 -0500

    Work on look script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     private CharacterController characterController;
-    private Vector2 inputs;
+    private Vector2 moveInputs;
+    private Vector2 mouseInputs;
+    public Vector2 sensitivity;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +22,39 @@ public class PlayerController : MonoBehaviour
     {
         updateInputs();
         applyMovement();
+        applyMouseRotation();
     }
 
     private void updateInputs()
     {
+        updateMoveInputs();
+        updateMouseInputs();
+    }
+
+    private void updateMouseInputs()
+    {
+        float horizontal = Input.GetAxis("Mouse X");
+        float vertical = Input.GetAxis("Mouse Y");
+        mouseInputs = new Vector2(horizontal, vertical);
+    }
+
+    private void updateMoveInputs()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        inputs = new Vector2(horizontal, vertical);
+        moveInputs = new Vector2(horizontal, vertical);
     }
 
     private void applyMovement()
     {
-        Vector3 movement = new Vector3(inputs.x, 0, inputs.y);
+        Vector3 movement = new Vector3(moveInputs.x, 0, moveInputs.y);
         characterController.Move(movement * Time.deltaTime * speed);
+    }
+
+    private void applyMouseRotation()
+    {
+        Vector3 lookRotation = new Vector3(mouseInputs.y, mouseInputs.x, 0);
+        lookRotation = (lookRotation * sensitivity) * Time.deltaTime;
+        characterController.transform.Rotate(lookRotation);
     }
 }
