8191d35aa0e553b1ba7a9f422284c449bfa862c1
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-20 20:16:50 -0500

    Camera Controller completed

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
 
     public float speed;
     private CharacterController characterController;
     private Vector2 moveInputs;
-    private Vector2 mouseInputs;
-    public Vector2 sensitivity;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +18,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        updateInputs();
-        applyMovement();
-        applyMouseRotation();
-    }
-
-    private void updateInputs()
-    {
         updateMoveInputs();
-        updateMouseInputs();
-    }
-
-    private void updateMouseInputs()
-    {
-        float horizontal = Input.GetAxis("Mouse X");
-        float vertical = Input.GetAxis("Mouse Y");
-        mouseInputs = new Vector2(horizontal, vertical);
+        applyMovement();
     }
 
     private void updateMoveInputs()
@@ -49,12 +33,5 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(moveInputs.x, 0, moveInputs.y);
         characterController.Move(movement * Time.deltaTime * speed);
-    }
-
-    private void applyMouseRotation()
-    {
-        Vector3 lookRotation = new Vector3(mouseInputs.y, mouseInputs.x, 0);
-        lookRotation = (lookRotation * sensitivity) * Time.deltaTime;
-        characterController.transform.Rotate(lookRotation);
     }
 }
