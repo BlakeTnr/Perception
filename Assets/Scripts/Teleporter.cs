02fabb1b4d92a5777d03fb60bc41a57060066309
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-24 11:12:46 -0500

    Teleporting with position & rotation finished

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -8,16 +8,46 @@ public class Teleporter : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        CharacterController player = other.GetComponent<CharacterController>();
-        teleport(player);
+        if (gameObject.GetComponent<Teleporter>().enabled)
+        {
+            CharacterController player = other.GetComponent<CharacterController>();
+            teleport(player);
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        gameObject.GetComponent<Teleporter>().enabled = true;
     }
 
     private void teleport(CharacterController player)
     {
+        fixInfiniteTeleporting();
+        portalPositionPlayer(player);
+        makePlayerLookOut(player);
+    }
+
+    private void fixInfiniteTeleporting()
+    {
+        otherPortal.GetComponent<Teleporter>().enabled = false;
+    }
+
+    private void portalPositionPlayer(CharacterController player)
+    {
         Vector3 relativeToEnterPortal = gameObject.transform.InverseTransformPoint(player.transform.position);
-        relativeToEnterPortal = Vector3.Scale(relativeToEnterPortal, new Vector3(1, 1, 1));
+        relativeToEnterPortal = flipVector3XZ(relativeToEnterPortal);
         Vector3 worldPositionExit = otherPortal.transform.TransformPoint(relativeToEnterPortal);
         setPlayerPosition(player, worldPositionExit);
+    }
+
+    private void makePlayerLookOut(CharacterController player)
+    {
+        player.transform.Rotate(new Vector3(0, 180, 0));
+    }
+
+    private Vector3 flipVector3XZ(Vector3 vector)
+    {
+        return Vector3.Scale(vector, new Vector3(-1, 1, -1));
     }
 
     private void setPlayerPosition(CharacterController player, Vector3 position)
