271cbfc24aefe4db4c0ccc4f7a8a88a767cef6ba
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-28 09:15:02 -0500

    Teleporter (#4)
    * Work on teleporter
    
    Issue with teleporter changing camera's transform and then the transform being overriden
    
    * Fixed character controller set position bug
    
    * Update Development1.unity
    
    * Work on portal teleport relativity
    
    * Teleporting with position & rotation finished
    
    * Rotate gravity
    
    * Character rotate gravity direction
    
    * Convert charactercontroller to rigidbody (#3)
    
    * Update MovementController.cs
    
    * Rigidbody custom gravity
    
    * Fixed naming mistake for custom gravity
    
    * Changing character rotation when teleported
    
    * Teleport rotation
    
    * Update Teleporter.cs
    
    * Changes gravity when teleported
    
    * Flipped x to make portals seamless
    
    This is because when you teleport through a portal it rotates you by 180 so you look out and when doing this it teleports you to what look like the wrong side of the portal

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public GameObject otherPortalView;
+    private GameObject otherPortal;
+    private GameObject gameObjectPortal;
+
+    void Start()
+    {
+        otherPortal = getPortalFromPortalView(otherPortalView);
+        gameObjectPortal = getPortalFromPortalView(gameObject);
+    }
+
+    private GameObject getPortalFromPortalView(GameObject portalView)
+    {
+        GameObject portalOutline = portalView.transform.parent.gameObject;
+        GameObject portal = portalOutline.transform.parent.gameObject;
+        return portal;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.GetComponent<Teleporter>().enabled)
+        {
+            teleport(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        gameObject.GetComponent<Teleporter>().enabled = true;
+    }
+
+    private void disableOtherPortal()
+    {
+        otherPortalView.GetComponent<Teleporter>().enabled = false;
+    }
+
+    private void teleport(GameObject player)
+    {
+        disableOtherPortal();
+        portalPositionplayer(player);
+        portalRotatePlayer(player);
+        rotateGravity(player.GetComponent<RigidBodyCustomGravity>());
+    }
+
+    private void portalPositionplayer(GameObject player)
+    {
+        movePlayerLocally(player);
+        flipX(player);
+        // should do some flipping sign of x or z (I forgot) to make seamless
+    }
+
+    private void movePlayerLocally(GameObject player)
+    {
+        Transform previousParent = player.transform.parent;
+        player.transform.parent = gameObjectPortal.transform;
+
+        Vector3 localPosition = player.transform.localPosition;
+        player.transform.parent = otherPortal.transform;
+        player.transform.localPosition = localPosition;
+
+        player.transform.parent = previousParent;
+    }
+
+    private void flipX(GameObject player) // Needs better name
+    {
+        Transform previousParent = player.transform.parent;
+        player.transform.parent = otherPortalView.transform;
+        Vector3 newLocalPosition = Vector3.Scale(player.transform.localPosition, new Vector3(-1, 1, 1));
+        player.transform.localPosition = newLocalPosition;
+        player.transform.parent = previousParent;
+    }
+
+    private void portalRotatePlayer(GameObject player)
+    {
+        GameObject camera = player.transform.Find("Camera").gameObject;
+        rotatePlayerLocally(player, camera);
+        rotateCameraOutwards(camera.transform);
+    }
+
+    private void rotatePlayerLocally(GameObject player, GameObject camera)
+    {
+        Transform previousParent = player.transform.parent;
+        player.transform.parent = gameObjectPortal.transform;
+
+        Quaternion localRotation = player.transform.localRotation;
+        player.transform.parent = otherPortal.transform;
+        player.transform.localRotation = localRotation;
+
+        player.transform.parent = previousParent;
+    }
+
+    private void rotateCameraOutwards(Transform camera)
+    {
+        Vector3 newRotation = camera.localRotation.eulerAngles + new Vector3(0, 180, 0);
+        camera.localRotation = Quaternion.Euler(newRotation);
+    }
+
+    private void rotateGravity(RigidBodyCustomGravity rigidBodyCustomGravity)
+    {
+        Vector3 gravityLocal = gameObjectPortal.transform.InverseTransformVector(rigidBodyCustomGravity.gravityForce);
+        Vector3 newGravity = otherPortal.transform.TransformVector(gravityLocal);
+        rigidBodyCustomGravity.gravityForce = newGravity;
+    }
+
+}
