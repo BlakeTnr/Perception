14fe818c6f179a08fc646a6d0777d7a45575045c
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-30 12:09:46 -0500

    Portal (#6)
    * Created portal object
    
    * Portal object fixing & optimising
    
    * Remade portal
    
    * Teleporter (#4)
    
    * Work on teleporter
    
    Issue with teleporter changing camera's transform and then the transform being overriden
    
    * Fixed character controller set position bug
    
    * Update Development1.unity
    
    * Work on portal teleport relativity
    
    * Teleporting with position & rotation finished
    
    * Rotate gravity
    
    * Character rotate gravity direction
    
    * Convert charactercontroller to rigidbody (#3)
    
    * Update MovementController.cs
    
    * Rigidbody custom gravity
    
    * Fixed naming mistake for custom gravity
    
    * Changing character rotation when teleported
    
    * Teleport rotation
    
    * Update Teleporter.cs
    
    * Changes gravity when teleported
    
    * Flipped x to make portals seamless
    
    This is because when you teleport through a portal it rotates you by 180 so you look out and when doing this it teleports you to what look like the wrong side of the portal
    
    * Camera movement fixed
    
    * Visual portal (#5)
    
    * Visual portal created and needs touching up
    
    * Portal View finished

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public GameObject player;
 
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Debug.Log(player.transform.position);
+        Debug.Log("c");
+    }
 }
